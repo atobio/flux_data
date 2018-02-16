f71d6e4f0dfb7295aad728436546bdb5e1f44592
atobio <atobio@colgate.edu>
2018-02-16 12:21:54 -0500

    updated script

--- fluxes_model_script.r
+++ fluxes_model_script.r
@@ -1,21 +1,109 @@
+#####################################################################
+###########Script created by A Tobio on 2/14/18. ####################
+###########The script runs a Bayesian model      ####################
+###########describing ET across stands in        ####################
+###########in response to environmental drivers  ####################
+#####################################################################
+
+#load libraries
 library(plyr)
 library(rjags)
 library(coda)
 library(mcmcplots)
+library(lubridate)
+#set directory
+#setwd("/Users/Ana/Documents/siberia_data/dg_fluxes")
+#read in data
+fluxes <- read.csv("z:\\student_research\\tobio\\all_fluxes_2015_16resave.csv")
+#canopy rh and temperature for 2016 data
+datRH <- read.csv("z:\\student_research\\tobio\\viperSensor\\decagon\\met\\RH.VP4.csv")
+datTC <- read.csv("z:\\student_research\\tobio\\viperSensor\\decagon\\met\\TempC.VP4.csv")
+#read in precip data
+datAirP <- read.csv("z:\\student_research\\tobio\\viperSensor\\airport\\airport.csv")
 
-setwd("/Users/Ana/Documents/siberia_data/dg_fluxes")
+#met under story for 2015
+datDAV <- read.csv("z:\\student_research\\tobio\\viperSensor\\met\\DAV.csv")
+datHDF1 <- read.csv("z:\\student_research\\tobio\\viperSensor\\met\\HDF1.csv")
+datLBR <- read.csv("z:\\student_research\\tobio\\viperSensor\\met\\LBR.csv")
+datLDF2 <- read.csv("z:\\student_research\\tobio\\viperSensor\\met\\LDF2.csv")
+datMDF1 <- read.csv("z:\\student_research\\tobio\\viperSensor\\met\\MDF1.csv")
+datMDF2 <- read.csv("z:\\student_research\\tobio\\viperSensor\\met\\MDF2.csv")
 
-fluxes <- read.csv("all_fluxes_2015_16.csv")
+#############################################
+#####organize fluxes data      ##############
+#############################################
 
+#check how many fluxes are in each stand
 f.count <- aggregate(fluxes$umol.h2o.m2.sec.1, by=list(fluxes$stand, fluxes$density), FUN="length")
-
 colnames(f.count) <- c("stand","density", "count")
 f.count$standID <- seq(1,dim(f.count)[1])
-
+#add stand id to the fluxes data
 fluxes2 <- join(fluxes, f.count, by=c("stand","density"), type="left")
 
-datalist <- list(Nobs=dim(fluxes2)[1], w.flux = fluxes2$umol.h2o.m2.sec.1, vpd = fluxes2$vpd, 
-                 standID = fluxes2$standID, Nstand=dim(f.count)[1])
+#############################################
+#####organize met data         ##############
+#############################################
+#subset and match
+datLRHmet <- data.frame(datRH[datRH$site=="ld",1:3], RH=datRH$RH.VP4[datRH$site=="ld"])
+datLTCmet <- data.frame(datTC[datTC$site=="ld",1:3], Temp=datTC$TempC.VP4[datTC$site=="ld"])
+
+datHRHmet <- data.frame(datRH[datRH$site=="hd",1:3], RH=datRH$RH.VP4[datRH$site=="hd"])
+datHTCmet <- data.frame(datTC[datTC$site=="hd",1:3], Temp=datTC$TempC.VP4[datTC$site=="hd"])
+
+datLmet <- join(datLRHmet, datLTCmet, by=c("doy","year","hour"),type="inner")
+datHmet <- join(datHRHmet, datHTCmet, by=c("doy","year","hour"),type="inner")
+
+datLmet$standID <- rep(5, dim(datLmet)[1])
+datHmet$standID <- rep(1, dim(datHmet)[1])
+# organize RH and temp data for 2015
+#first convert dates
+dateDAV <- as.Date(datDAV[,1], "%m/%d/%Y %H:%M") 
+datDAVmet <- data.frame(doy=yday(dateDAV), year=year(dateDAV), hour=datDAV$hour, RH=datDAV$VP.3.Humidity.Temp,Temp=datDAV$VP.3.Humidity.Temp.1, standID=rep(1, dim(datDAV)[1]))
+dateHDF1 <- as.Date(datHDF1[,1], "%m/%d/%Y %H:%M") 
+datHDF1met <- data.frame(doy=yday(dateHDF1), year=year(dateHDF1), hour=datHDF1$hour, RH=datHDF1$VP.3.Humidity.Temp,Temp=datHDF1$VP.3.Humidity.Temp.1, standID=rep(2, dim(datHDF1)[1]))
+dateLBR<- as.Date(datLBR[,1], "%m/%d/%Y %H:%M") 
+datLBRmet <- data.frame(doy=yday(dateLBR), year=year(dateLBR), hour=datLBR$hour, RH=datLBR$VP.3.Humidity.Temp,Temp=datLBR$VP.3.Humidity.Temp.1, standID=rep(4, dim(datLBR)[1]))
+dateLDF2 <- as.Date(datLDF2[,1], "%m/%d/%Y %H:%M") 
+datLDF2met <- data.frame(doy=yday(dateLDF2), year=year(dateLDF2), hour=datLDF2$hour, RH=datLDF2$VP.3.Humidity.Temp,Temp=datLDF2$VP.3.Humidity.Temp.1, standID=rep(5, dim(datLDF2)[1]))
+dateMDF1 <- as.Date(datMDF1[,1], "%m/%d/%Y %H:%M") 
+datMDF1met <- data.frame(doy=yday(dateMDF1), year=year(dateMDF1), hour=datMDF1$hour, RH=datMDF1$VP.3.Humidity.Temp,Temp=datMDF1$VP.3.Humidity.Temp.1, standID=rep(7, dim(datMDF1)[1]))
+dateMDF2 <- as.Date(datMDF2[,1], "%m/%d/%Y %H:%M") 
+datMDF2met <- data.frame(doy=yday(dateMDF2), year=year(dateMDF2), hour=datMDF2$hour, RH=datMDF2$VP.3.Humidity.Temp,Temp=datMDF2$VP.3.Humidity.Temp.1, standID=rep(8, dim(datMDF2)[1]))
+
+datHDRmet <- data.frame(datDAVmet[,1:5], standID=rep(3, dim(datDAV)[1]))
+datLDF3met <- data.frame(datLDF2met[,1:5], standID=rep(6, dim(datLDF2)[1]))
+datMDF4met <- data.frame(datMDF1met[,1:5], standID=rep(9, dim(datMDF1)[1]))
+
+
+datMet<- rbind(datLmet,datHmet, datDAVmet,datHDF1met,datLBRmet,datLDF2met,datMDF1met,datMDF2met,datHDRmet,datLDF3met,datMDF4met)
+
+datMet$e.sat <- 0.611*exp((17.502*datMet$Temp)/(datMet$Temp+240.97))
+datMetRHfix<-ifelse(datMet$RH>=1,.999,datMet$RH)
+datMet$VPD <- (datMet$e.sat-(datMetRHfix*datMet$e.sat))
+
+##################################################
+##################################################
+#####combine met   ###############################
+##################################################
+
+head(fluxes2)
+
+
+fluxes2$hourR <- round_any(fluxes2$hour, 0.5)
+
+colnames(datMet)[3] <- "hourR"
+
+
+fluxes3 <- join(fluxes2, datMet, by=c("doy","year","hourR","standID"), type="left")
+
+
+#############################################
+#####set up model run          ##############
+#############################################
+
+
+datalist <- list(Nobs=dim(fluxes3)[1], w.flux = fluxes3$umol.h2o.m2.sec.1, vpd = fluxes3$VPD, 
+                 standID = fluxes3$standID, Nstand=dim(f.count)[1])
 
 parms <- c("b.0", "b.1", "sig.flux","w.rep")
 
@@ -33,4 +121,3 @@ write.table(mod.out$statistics, "C:\\Users\\Ana\\Documents\\siberia_data\\model_
             sep=",")
 write.table(mod.out$quantiles, "C:\\Users\\Ana\\Documents\\siberia_data\\model_output\\run1\\mod_quantile.csv",
             sep=",")
-
