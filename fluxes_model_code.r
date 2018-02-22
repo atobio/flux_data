4a364c2c2c24f951b365ec748efe96188e9568ea
atobio <atobio@colgate.edu>
2018-02-22 15:58:20 -0500

    checked into how well the model is performing

--- fluxes_model_code.r
+++ fluxes_model_code.r
@@ -4,8 +4,8 @@ model{
 #likelihood 
 for(i in 1:Nobs){
   # calculate mean for standDay
-  w.flux[i] ~ dnorm(mu.flux[i], tau.flux)
-  mu.flux[i] <- b.0[densityID[i]] + b.1[densityID[i]] * log(vpd[i]) 
+  w.flux[i] ~ dnorm(mu.flux[i], tau.flux[densityID[i]])
+  mu.flux[i] <- b.0[densityID[i]] + b.1[densityID[i]] * vpd[i] 
 }
   
   
@@ -13,15 +13,14 @@ for(i in 1:Nobs){
   
 #assign priors
 for(j in 1:Ndensity){
-  b.0[j] ~ dnorm(0,0.001)
+  b.0[j] ~ dnorm(0,0.1)
 
-  b.1[j] ~ dnorm(0,0.001) 
- 
-}
+  b.1[j] ~ dnorm(0,0.1)
+  
+  tau.flux[j] <- pow(sig.flux[j], -2)
+  
+  sig.flux[j] ~ dunif(0,50000)
+ }
 
-
-tau.flux <- pow(sig.flux, -2)
-
-sig.flux ~ dunif(0,50000)
 
 }
