fc8088237aaf937dfa00ce98999ad0650cd899fb
atobio <atobio@colgate.edu>
2018-02-22 15:24:45 -0500

    averaged values across stand and day

--- fluxes_model_code.r
+++ fluxes_model_code.r
@@ -3,11 +3,14 @@ model{
 
 #likelihood 
 for(i in 1:Nobs){
+  # calculate mean for standDay
   w.flux[i] ~ dnorm(mu.flux[i], tau.flux)
   mu.flux[i] <- b.0[densityID[i]] + b.1[densityID[i]] * log(VPD[i]) + b.2[densityID[i]] * pr.ave[i] + b.3[densityID[i]] * par[i] 
-  w.rep[i] ~ dnorm(mu.flux[i], tau.flux)
 }
+  
+  
 
+  
 #assign priors
 for(j in 1:Ndensity){
   b.0[j] ~ dnorm(0,0.001)
@@ -16,7 +19,8 @@ for(j in 1:Ndensity){
   b.3[j] ~ dnorm(0,0.001) 
 }
 
-tau.flux <- pow(sig.flux,-2)
+
+tau.flux <- pow(sig.flux, -2)
 
 sig.flux ~ dunif(0,50000)
 
